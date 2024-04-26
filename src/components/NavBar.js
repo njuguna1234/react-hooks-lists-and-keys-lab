b6b16bfa6be770566193ad8e5cb1e00d4c7d21c4
roy wahome <roy.njuguna@student.moringaschool.com>
2024-04-26 13:24:33 +0300

    athe final commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const list = links.map((name, index) => (
+    <nav key={index}>
+      <a href={`#${name}`}>{name}</a>
+    </nav>
+  ));
+
+  return <div>{list}</div>;
 }
 
 export default NavBar;
